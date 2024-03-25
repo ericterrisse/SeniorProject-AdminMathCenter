8041249cd2149cd2ef1843ea82cfc3a81ee4db45
bla1232 <92119509+bla1232@users.noreply.github.com>
2024-03-25 02:33:16 -0500

    Successfully adds post data to db
    -need to add way to only allow check-in page to call api

--- src/app/api/catch/route.ts
+++ src/app/api/catch/route.ts
@@ -1,50 +1,64 @@
-import { Student, ClassName, Prisma, PrismaClient } from '@prisma/client'
+import {ClassName, Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
+const cors = require('cors');
+
 
 const prisma = new PrismaClient()
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
-app.post(`/api/catch`, async (req, res) => {
-    try{
-        const { fullname, studentID, classId } = req.body;
-        //splitting up full name to match database params
-        const [firstName, ...lastNameParts] = fullname.split(' ');
-        const lastName = lastNameParts.join(' ');
-        const studentIDP = parseInt(studentID);
-
-        const Student = await prisma.student.create({
-            data: {
+app.post('/api/catch', async (req, res) => {
+    try {
+      //data in this order
+      const { fullname, classId , studentId } = req.body;
+  
+      //split up first and last name from 'fullname'
+      const [firstName, ...lastNameParts] = fullname.split(' ');
+      const lastName = lastNameParts.join(' ');
+  
+      //find student with the unique studentId
+      const student = await prisma.student.findUnique({
+        where: { studentId },
+      });
+  
+      //create student if it doesn't already exist
+      if (!student) {
+        const newStudent = await prisma.student.create({
+          data: {
+            studentId,
             firstName,
             lastName,
-            },
+          },
         });
-
-
-        const studentTracker = await prisma.studentTracker.create({
-            data: {
-                className: classId as ClassName,
-                checkInTime: new Date(),
-
-                studentId: Student.id,//fix this
-                //Student: {
-                //  connect: { id: Student.id },  
-                //},
-                //
-                //have to figure out how to make the below code work
-                //studentId: parseInt(studentID),
-            },
-        });
-        res.json(studentTracker)
-    }catch(error){
-        res.status(500).json({ error: "Error 500" });
-    }
-
   
-});
+        //making studentTracker with relationship to newStudent
+        await prisma.studentTracker.create({
+          data: {
+            className: classId as ClassName,
+            checkInTime: new Date(),
+            student: { connect: { id: newStudent.id } },
+          },
+        });
+      } else {
 
-//running on different port bc it works for some reason
+        //making studentTracker with relationship with existing student
+        await prisma.studentTracker.create({
+          data: {
+            className: classId as ClassName,
+            checkInTime: new Date(),
+            student: { connect: { id: student.id } },
+          },
+        });
+      }
+  
+      res.status(200).json({ message: 'check-in successful' }); 
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'error 500' });
+    }
+  });
+  
 const server = app.listen(3006, () =>
   console.log(`Server ready at: http://localhost:3006`),
 )
