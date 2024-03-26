b91abf073bc3140ae5775599ca001518d0afc1e2
bla1232 <92119509+bla1232@users.noreply.github.com>
2024-03-26 09:02:09 -0500

    added fetch

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-import data from "@/lib/fake_data.json";
+"use client";
+import React, { useEffect, useState } from "react";
+//import data from "@/lib/fake_data.json";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import BarChar from "./BarChar";
 import Item from "./Item";
 import ItemPicker from "./ItemPicker";
 
-interface Props {}
+interface StudentData {
+	fullname: string;
+	StudentTracker: { className: string; checkInTime: string}[];
+}
+const Dashboard = () => {
+	const [students, setStudents] = useState<StudentData[]>([]);
 
-const Dashboard = (props: Props) => {
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+			const response = await fetch('http://localhost:3007/api/display');
+			if (!response.ok) {
+				throw new Error('failed to get data');
+			}
+			const data = await response.json();
+			setStudents(data);
+			} catch (error) {
+				console.error('error:', error);
+			}
+		};
+	fetchData();
+	}, []);
+
 	return (
 		<div className="p-10 flex flex-col gap-10">
 			<div className="flex gap-10">
@@ -28,12 +49,11 @@ const Dashboard = (props: Props) => {
 			</div>
 			<div className="flex gap-10">
 				<div className="w-3/5">
-					<DataTable columns={columns} data={data} />
+					<DataTable columns={columns} data={students} />
 				</div>
 				<BarChar />
 			</div>
 		</div>
 	);
 };
-
 export default Dashboard;
